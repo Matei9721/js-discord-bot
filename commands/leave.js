c589cc2256216c10b0263140ea9292be550191eb
Luca <spataruluca@gmail.com>
2023-05-06 22:21:38 +0200

    Slash commands implementation #2

--- commands/leave.js
+++ commands/leave.js
@@ -1,19 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const sleep = require('../sleepFunc');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('leave')
         .setDescription('Make the bot leave from the voice channel'),
     async execute(interaction, client) {
-        if(client.botMap.has(interaction.guild.id)) {
-            let connection = client.botMap.get(interaction.guild.id).connection
-            connection.destroy();
-            connection = null;
-            client.botMap.get(interaction.guild.id).connection = null;
-            client.botMap.get(interaction.guild.id).queue = [];
-            client.botMap.get(interaction.guild.id).setPlayer();
+        //Indicate that the command is being processed
+        interaction.reply({ content: 'Loading the song(s) for you..', ephemeral: true }); 
+        sleep(5).then(() => { interaction.deleteReply() })
+        
+        if(client.botMap.has(interaction.channel.guild.id)) {
+            if (client.botMap.get(interaction.channel.guild.id).connection) {
+                let connection = client.botMap.get(interaction.channel.guild.id).connection
+                connection.destroy()
+                connection = null;
+            }
+            client.botMap.get(interaction.channel.guild.id).connection = null;
+            client.botMap.get(interaction.channel.guild.id).queue = [];
+            client.botMap.get(interaction.channel.guild.id).setPLayer();
         }
-        interaction.reply("Gusi?");
-        interaction.deleteReply();
     },
 };
